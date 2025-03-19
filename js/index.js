af632e9e4873022a9730a3033168ad87e20b6b1a
shadidul-islam <qazi.sadidul@gmail.com>
2025-03-20 02:41:07 +0600

    Class added: Part-4

---
+++ js/index.js
@@ -0,0 +1,43 @@
+import { Todos } from "./class/Todos.js";
+
+const BACKEND_ROOT_URL = 'http://localhost:3001';
+const todos = new Todos(BACKEND_ROOT_URL);
+
+const list = document.querySelector('ul');
+const input = document.querySelector('input');
+
+input.disabled = true;
+
+const renderTask = (task) => {
+    const li = document.createElement('li');
+    li.setAttribute('class', 'list-group-item');
+    li.innerHTML = task.getText();
+    list.appendChild(li);
+};
+
+const getTasks = () => {
+    todos.getTasks().then((tasks) => {
+        tasks.forEach(task => {
+            renderTask(task);
+        });
+        input.disabled = false;
+    }).catch((error) => {
+        alert(error);
+    });
+};
+
+input.addEventListener('keypress', (event) => {
+    if (event.key === 'Enter') {
+        event.preventDefault();
+        const task = input.value.trim();
+        if (task !== '') {
+            todos.addTask(task).then((task) => {
+                renderTask(task);
+                input.value = '';
+                input.focus();
+            });
+        }
+    }
+});
+
+getTasks();
